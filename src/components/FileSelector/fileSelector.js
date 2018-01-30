b657c5656481352ff408f364782f8120c5ed8859
Manoj Sethi <msethi3@sapient.com>
2018-01-30 22:08:34 +0000

    prod config

--- src/components/FileSelector/fileSelector.js
+++ src/components/FileSelector/fileSelector.js
@@ -1,35 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './styles.scss'
 import PropTypes from 'prop-types';
 
-class FileSelector extends Component {
+const FileSelector = (props) => {
 
+    const {handleFileSelection, error} = props;
 
+    return (<div>
+            <div>
+                {error}
+            </div>
+            <div className="upload">
 
-//
-    render() {
-        const {handleFileSelection, handleUpload, error} = this.props;
-
-        return (<div>
-                <div>
-                    {error}
-                </div>
-                <div className="upload">
-
-                    <label htmlFor="file-upload" className="custom-file-upload alert alert-info">
-                        <i className="fa fa-cloud-upload"/> Select File(s) to upload
-                    </label>
-
-                </div>
-
-                <input id="file-upload" accept="image/*,video/mp4,video/x-m4v,video/*" type="file" onChange={(e) => {
-                    handleFileSelection(e.target.files, e.target.files.length);
-                }} multiple/>
+                <label htmlFor="file-upload" className="custom-file-upload alert alert-info">
+                    <i className="fa fa-cloud-upload"/> Select File(s) to upload
+                </label>
 
             </div>
-        );
-    }
-}
+
+            <input id="file-upload" accept="image/*,video/mp4,video/x-m4v,video/*" type="file" onChange={(e) => {
+                handleFileSelection(e.target.files, e.target.files.length);
+            }} multiple/>
+
+        </div>
+    );
+};
 
 
 FileSelector.propTypes = {
